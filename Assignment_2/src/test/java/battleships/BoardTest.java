dbacb888b1c97af03568a6a0080e591f827ffe04
Shiro <wretched_egg@hotmail.de>
2020-10-15 18:23:24 +0200

    Changed project structure

---
+++ Assignment_2/src/test/java/battleships/BoardTest.java
@@ -0,0 +1,14 @@
+package battleships;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardTest {
+
+    @Test
+    public void testHelloWorld(){
+        System.out.println("Hi");
+    }
+
+}
